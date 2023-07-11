6b7b7fc902ca87c679237056d73f910433f4f730
dev <ivh@live.com>
2023-07-12 00:39:21 +0800

    first commit

--- cmd/oss/main.go
+++ cmd/oss/main.go
@@ -6,17 +6,21 @@ import (
 
 	"github.com/kzaun/novel/internal/oss"
 	"github.com/kzaun/novel/pkg/fx/ginfx"
+	"github.com/kzaun/novel/pkg/fx/knadhfx"
+	"github.com/kzaun/novel/pkg/fx/logfx"
 	"github.com/kzaun/novel/pkg/fx/miniofx"
 
-	// "github.com/go-kit/kit/transport/http"
 	"go.uber.org/fx"
 )
 
 func main() {
 	app := fx.New(
-		miniofx.NewMinIOModule(),
-		ginfx.NewGinModule(),
-		fx.Invoke(oss.RegisterHandlers),
+		miniofx.MinioModule,
+		ginfx.GinModule,
+		oss.OssModule,
+		knadhfx.ConfigModule,
+		logfx.LogModule,
+		fx.Invoke(oss.Register),
 	)
 
 	if err := app.Start(context.Background()); err != nil {
@@ -25,5 +29,3 @@ func main() {
 
 	<-app.Done()
 }
-
-
