578aaf6418cc5c9bd31ddbaa92daa6479bdc8fb7
Xia <xiajinxue@sharkgulf.com>
2023-07-13 15:49:41 +0800

    route position

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
+
+type Novel struct {
+	Title       string
+	Author      string
+	Description string
+	Link        string
+}
+
+func main() {
+	c := colly.NewCollector()
+
+	c.OnHTML(".list .item", func(e *colly.HTMLElement) {
+		novel := Novel{
+			Title:       e.ChildText(".title"),
+			Author:      e.ChildText(".author"),
+			Description: e.ChildText(".desc"),
+			Link:        e.Request.AbsoluteURL(e.ChildAttr("a", "href")),
+		}
+		fmt.Println(novel)
+	})
+
+	c.Visit("https://www.quanben.io/")
+}
--- internal/scraper/endpoint.go
+++
@@ -1,7 +0,0 @@
-package scraper
-
-import "context"
-
-type Scraper interface {
-	Scrape(ctx context.Context, url string) (result string, err error)
-}
